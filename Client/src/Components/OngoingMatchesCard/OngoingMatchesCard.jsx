f72f53c1e8c20ed87a64d4c9455aa03381c8593c
Bhagya-Gunaratne <10898473@students.plymouth.ac.uk>
2025-03-20 23:21:19 +0000

    Ongoing Matches Section Completed

--- Client/src/Components/OngoingMatchesCard/OngoingMatchesCard.jsx
+++ Client/src/Components/OngoingMatchesCard/OngoingMatchesCard.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
 import './OngoingMatchesCard.css'
-import sampleImage from '../../assets/ongoing-sample1.png'
+import OngoingMatches from '../../assets/DummyData/OngoingMatches.js'
 
 const OngoingMatchesCard = () => {
+
     return (
         <div className='ongoing-container'>
             <div className="ongoing-cont-title"><h1>ONGOING MATCHES</h1></div>
             <div className="ongoin-cards">
-                <div className="ongoin-card">
-                    <div className="ongoing-c-img">
-                        <img src={sampleImage} alt="ongoing-card-image" />
+
+                {OngoingMatches.map((ongMatch) => (
+                    <div className="ongoin-card" key={ongMatch.id}>
+                        <div className="ongoing-c-img">
+                            <img src={ongMatch.image} alt="ongoing-match" />
+                        </div>
+                        <div className="ongoing-c-info">
+                            <h2>{ongMatch.teams} <br></br> {ongMatch.date}</h2>
+                        </div>
                     </div>
-                    <div className="ongoing-c-info">
-                        <h2>Aitken Spence vs. <br></br >WSO2 Lanka - 24 FEB 2025</h2>
-                    </div>
-                </div>
-                <div className="ongoin-card">2</div>
-                <div className="ongoin-card">3</div>
-                <div className="ongoin-card">4</div>
+                ))}
+
             </div>
             <div className="ongoing-cont-extend"><i class="fa-solid fa-angle-down"></i></div>
         </div>
     )
 }
-import './OngoingMatchesCard.css'
 
 export default OngoingMatchesCard
---
+++ Client/src/assets/DummyData/OngoingMatches.js
@@ -0,0 +1,33 @@
+import sampleImage1 from '../ongoing-sample1.png';
+import sampleImage2 from '../ongoing-sample2.png';
+import sampleImage3 from '../ongoing-sample3.png';
+import sampleImage4 from '../ongoing-sample4.png';
+
+const OngoingMatches = [
+    {
+        id: 1,
+        image: sampleImage1,
+        teams: "Aitken Spence vs. WSO2 Lanka",
+        date: "24 FEB 2025"
+    },
+    {
+        id: 2,
+        image: sampleImage2,
+        teams: "SL Air Force Eagles vs. Kings BC",
+        date: "28 FEB 2025"
+    },
+    {
+        id: 3,
+        image: sampleImage3,
+        teams: "Daraz vs. Dialog Axiata",
+        date: "26 FEB 2025"
+    },
+    {
+        id: 4,
+        image: sampleImage4,
+        teams: "CSL Air Force vs. Maldivian",
+        date: "02 MAR 2025"
+    }
+];
+
+export default OngoingMatches;
