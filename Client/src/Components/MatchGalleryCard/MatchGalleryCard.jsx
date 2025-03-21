bad6f4715559514e9945315ffc1ac4529e1188b8
Bhagya-Gunaratne <10898473@students.plymouth.ac.uk>
2025-03-21 22:33:18 +0000

    Match Gallery Card Completed

--- Client/src/Components/MatchGalleryCard/MatchGalleryCard.jsx
+++ Client/src/Components/MatchGalleryCard/MatchGalleryCard.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './MatchGalleryCard.css'
+import MatchGalleryCards from '../../assets/DummyData/MatchGalleryCards'
 
 const MatchGalleryCard = () => {
   return (
     <div className='match-gallery-container'>
       <div className="matchglry-cont-title"><h1>MATCH GALLERY</h1></div>
+      <div className="matchglry-cards">
+
+        {MatchGalleryCards.map((mGal) => (
+          <div className="matchglry-card" key={mGal.id}>
+            <div className="matchglry-c-img">
+              <img src={mGal.image} alt="Match Gallery Card Image" />
+            </div>
+            <div className="matchglry-c-info">
+              <h2>{mGal.name}</h2>
+            </div>
+          </div>
+        ))}
+
+      </div>
+      <div className="ongoing-cont-extend"><i class="fa-solid fa-angle-down"></i></div>
 
     </div>
   )
---
+++ Client/src/assets/DummyData/MatchGalleryCards.js
@@ -0,0 +1,53 @@
+import img1 from '../mglery-1.png'
+import img2 from '../mglery-2.png'
+import img3 from '../mglery-3.png'
+import img4 from '../mglery-4.png'
+import img5 from '../mglery-5.png'
+import img6 from '../mglery-6.png'
+import img7 from '../mglery-7.png'
+import img8 from '../mglery-8.png'
+
+const MatchGalleryCards = [
+    {
+        id: 1,
+        image: img1,
+        name: "Sri Lanka vs. India – FIBA U18 Asia Cup 2024 SABA Qualifiers Final"
+    },
+    {
+        id: 2,
+        image: img2,
+        name: "St. Joseph’s College vs. St. Peter’s College – All Island Under-20 Schools Basketball Final"
+    },
+    {
+        id: 3,
+        image: img3,
+        name: "Sri Lanka vs. Bangladesh – Four-Nation FIBA U18 Asia Cup 2024 SABA Qualifiers"
+    },
+    {
+        id: 4,
+        image: img4,
+        name: "Seylan Bank vs. Fair First – Mercantile Basketball Tournament"
+    },
+    {
+        id: 5,
+        image: img5,
+        name: "St. Joseph’s College vs. St. Benedict’s College – Schools Basketball Championship"
+    },
+    {
+        id: 6,
+        image: img6,
+        name: "Sri Lanka vs. Nepal – Four-Nation FIBA U18 Asia Cup 2024 SABA Qualifiers"
+    },
+    {
+        id: 7,
+        image: img7,
+        name: "Sri Lanka vs. Republic of Korea – FIBA 3x3 Asia Cup 2024"
+    },
+    {
+        id: 8,
+        image: img8,
+        name: "Sri Lanka Sports Fiesta 2024 – 3x3 Basketball Championships"
+    },
+];
+
+export default MatchGalleryCards;
