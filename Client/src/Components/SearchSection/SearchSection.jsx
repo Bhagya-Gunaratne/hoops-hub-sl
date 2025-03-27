d83d0c3472adba57be8b4660bdc9fd307d93ccaf
Bhagya-Gunaratne <10898473@students.plymouth.ac.uk>
2025-03-27 23:57:14 +0000

    Match Gallery Page Initiation

---
+++ Client/src/Components/SearchSection/SearchSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchSection = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SearchSection
---
+++ Client/src/Pages/MatchGallery.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MatchGallery = () => {
+  return (
+    <div className='matchgallery-page-container'>
+      
+    </div>
+  )
+}
+
+export default MatchGallery
