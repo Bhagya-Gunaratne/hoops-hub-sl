7734dad2144ee34b250a64cb1610d59986eb4f3d
Bhagya-Gunaratne <10898473@students.plymouth.ac.uk>
2025-03-29 17:54:53 +0000

    Match Gallery Page Completed

--- Client/src/Pages/MatchGallery.jsx
+++ Client/src/Pages/MatchGallery.jsx
@@ -2,12 +2,51 @@ import React from 'react'
 import SearchSection from '../Components/SearchSection/SearchSection'
 import './MatchGallery.css'
 import NavBar from '../Components/NavBar/NavBar'
+import MatchGalleryData from '../assets/DummyData/MatchGalleryData'
+import Footer from '../Components/Footer/Footer'
 
 const MatchGallery = () => {
   return (
     <div className='matchgallery-page-container'>
       <NavBar />
       <SearchSection />
+
+      <div className="result-n-sort">
+        <h2>• Search Results:</h2>
+
+        <div className="sortsec">
+        <h2>SORT:</h2>
+          <select>
+            <option value="latest">Latest First</option>
+            <option value="oldest">Oldest First</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="match-gallery-sec">
+        {MatchGalleryData.map((match) => (
+          <div className="mtch-gal-single" key={match.id}>
+            <div className="mtch-gl-top">
+              <div className="gl-tp-left">
+                <img src={match.image} alt={match.name} />
+              </div>
+              <div className="gl-tp-right">
+                <img src={match.image} alt="Gallery Card Image" />
+                <div className="gl-inf-set">
+                  <h1>{match.name}</h1>
+                  <h2>{match.date}</h2>
+                  <h3>{match.place}</h3>
+                </div>
+              </div>
+            </div>
+            <div className="mtch-gl-bottom">
+              <p dangerouslySetInnerHTML={{ __html: match.parag }}></p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <Footer/>
     </div>
   )
 }
---
+++ Client/src/assets/DummyData/MatchGalleryData.js
@@ -0,0 +1,42 @@
+import img1 from '../mtg1.png'
+import img2 from '../mtg2.png'
+import img3 from '../mtg3.png'
+import img4 from '../mtg4.png'
+
+
+const MatchGalleryData = [
+    {
+        id: 1,
+        image: img1,
+        name: "Seylan Bank crowned C Division Champions",
+        date: "21/02/2025",
+        place: "Henry Pedris Basketball Complex",
+        parag: "Seylan Bank got off to a solid start scoring 24 points to Ranawana’s 17 points in the first quarter but were unable to keep up the scoring in the subsequent quarters. Dion Muthukumarana and Gayan Samaraweera were the main offensive players for Seylan Bank scoring 19 points each for their team. Captain Dilan Perera was the highest scorer for Ranawana securing 18 points for his team. <br /> <br /> Ranawana came back strongly in the second quarter after being outscored by 7 points in the first quarter. It was during this quarter that Ranawana..."
+    },
+    {
+        id: 2,
+        image: img2,
+        name: "Commercial Credit vs Seylan Bank | A Division | Women’s",
+        date: "18/02/2025",
+        place: "Royal College Indoor Complex",
+        parag: "The success of the tournament was further amplified by the continued support of My Cola Beverages, serving as the Platinum Sponsor for the third consecutive year. This partnership underscores the strong ties between Sri Lanka’s corporate sector and the MSBA in promoting sports and teamwork. <br /> <br /> As the 32nd MSBA League Tournament concludes, it leaves a lasting legacy of sportsmanship, corporate camaraderie, and basketball excellence. The event not only highlighted the skill and dedication of Sri Lanka’s corporate athletes but also set the stage for even greater achievements in the years to come..."
+    },
+    {
+        id: 3,
+        image: img3,
+        name: "MAS Holdings vs Seylan Bank – 31st Mercantile Basketball League ",
+        date: "04/02/2025",
+        place: "Royal College Sports Complex",
+        parag: "This year’s MSBA League Tournament featured a total of 636 registered players competing in 122 games, solidifying its reputation as Sri Lanka’s premier corporate basketball event. The MSBA has long been a platform for athletes to bridge their passion for basketball with their professional careers, with many players progressing to represent Sri Lanka on international stages. In a groundbreaking initiative, the tournament also invited Sri Lanka’s U18 Boys’ and Girls’ teams to compete in the Men’s and Women’s A Divisions. This decision provided young talent with invaluable exposure to high-level competition, furthering the MSBA’s mission to nurture..."
+    },
+    {
+        id: 4,
+        image: img4,
+        name: "Seylan Bank and Fair First Tourenment",
+        date: "30/01/2025",
+        place: "Royal College Indoor Complex",
+        parag: "The tournament finals were a spectacular culmination of skill and determination. In the Women’s A Division, Seylan Bank emerged victorious, narrowly defeating Commercial Credit with a final score of 56-50. The team showcased remarkable teamwork and resilience to secure their championship title. Meanwhile, in the Men’s A Division, Fairfirst Insurance delivered a commanding performance, defeating Commercial Credit 78-56 to claim the top honors. Praneeth Udumalagala of Fairfirst Insurance was recognized for his outstanding efforts throughout the tournament, earning him the title of Best Player of the Tournament..."
+    },
+];
+
+export default MatchGalleryData;
