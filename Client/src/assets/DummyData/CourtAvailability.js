e2037b34ef7e01853094f67134aad1ddf9e38bf4
Bhagya-Gunaratne <10898473@students.plymouth.ac.uk>
2025-04-07 10:32:49 +0000

    Update

---
+++ Client/src/assets/DummyData/CourtAvailability.js
@@ -0,0 +1,40 @@
+const CourtAvailability = [
+    {
+        id: 1,
+        courtId: 1,
+        name: "Trinity College Kandy Quadrangle",
+        availability: "available"
+    },
+    {
+        id: 2,
+        image: img2,
+        name: "St. Anthony’s College Basketball Court",
+        availability: "unavailable"
+    },
+    {
+        id: 3,
+        image: img3,
+        name: "Kingswood College Basketball Court",
+        availability: "available"
+    },
+    {
+        id: 4,
+        image: img4,
+        name: "Dharmaraja College Basketball Court",
+        availability: "available"
+    },
+    {
+        id: 5,
+        image: img5,
+        name: "Gateway College Kandy Basketball Court",
+        availability: "unavailable"
+    },
+    {
+        id: 6,
+        image: img6,
+        name: "Peradeniya University Outdoor Court",
+        availability: "unavailable"
+    },
+];
+
+export default CourtAvailability;
